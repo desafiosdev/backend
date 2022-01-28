d483de315cb1cc5f1075d64707706b0ce3dd5f1f
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-01-28 15:57:09 -0300

    test: ensure controller cadastrarusuarios exists

---
+++ tests/unit/controllers/cadastrar-usuarios-controller.spec.ts
@@ -0,0 +1,7 @@
+import CadastrarUsuariosController from '../../../src/controllers/cadastrar-usuarios-controller';
+
+describe('CadastrarUsuariosController', () => {
+  test('should exist', () => {
+    expect(CadastrarUsuariosController).toBeDefined();
+  })
+});
