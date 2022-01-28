6431f33c7a924bc940446c96ddd8cc96b1ad865d
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-01-28 15:51:02 -0300

    test: ensure homecontroller handle will receive a request and a response

--- tests/unit/controllers/home-controller.spec.ts
+++ tests/unit/controllers/home-controller.spec.ts
@@ -3,5 +3,18 @@ import HomeController from '../../../src/controllers/home-controller';
 describe('HomeController', () => {
   test('should exist', () => {
     expect(HomeController).toBeDefined();
-  });
-});
+  })
+  test('should have a method called handle', () => {
+    const sut = new HomeController();
+
+    expect(sut.handle).toBeDefined();
+  })
+  test('should receive a request and response', () => {
+    const sut = new HomeController();
+    const spy = jest.spyOn(sut, 'handle');
+
+    sut.handle('request', 'response');
+
+    expect(spy).toBeCalledWith('request', 'response');
+  })
+})
