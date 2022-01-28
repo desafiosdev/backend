2a24e1cf7efec4b57805ef06443d7d4b94f9fcd9
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-01-28 15:59:30 -0300

    test: ensure controller receive request and response on its handle method

--- tests/unit/controllers/cadastrar-usuarios-controller.spec.ts
+++ tests/unit/controllers/cadastrar-usuarios-controller.spec.ts
@@ -9,4 +9,12 @@ describe('CadastrarUsuariosController', () => {
 
     expect(sut.handle).toBeDefined();
   })
+  test('should receive a request and response', () => {
+    const sut = new CadastrarUsuariosController();
+    const spy = jest.spyOn(sut, 'handle');
+
+    sut.handle('request', 'response');
+
+    expect(spy).toBeCalledWith('request', 'response');
+  })
 });
