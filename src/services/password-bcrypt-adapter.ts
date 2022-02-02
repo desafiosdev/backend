5919ec0041bef17774130763c894c720fd24fcf0
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-02-02 09:24:18 -0300

    feat: adiciona bscrypt adapter

---
+++ src/services/password-bcrypt-adapter.ts
@@ -0,0 +1,30 @@
+import IPasswordCrypt from '../contracts/services/i-password-crypt';
+import * as bcrypt from 'bcrypt';
+
+export default class PasswordBcryptAdapter implements IPasswordCrypt {
+  private readonly saltRounds: number = 10;
+
+  async encrypt(password: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      bcrypt.hash(password, this.saltRounds, (err, hash) => {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve(hash);
+      });
+    });
+  }
+
+  async compare(password: string, encryptedPassword: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, encryptedPassword, (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve(result);
+      });
+    });
+  }
+}
