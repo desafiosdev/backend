acbfe09cb25ceb1881e0b50f2fc8126dbdeb602f
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-02-10 10:17:48 -0300

    feat: create cadastrar usuario factory

--- src/factories/cadastrar-usuario.ts
+++ src/factories/cadastrar-usuario.ts
@@ -2,9 +2,11 @@ import CadastrarUsuarioController from '../controllers/cadastrar-usuario-control
 import { Controller } from '../protocols/controller';
 import CadastrarUsuariosRepository from '../repositories/memory/cadastrar-usuarios-memory-repository';
 import CadastrarUsuariosService from '../services/cadastrar-usuarios-service';
+import PasswordCryptMemory from '../services/memory/password-crypt-memory';
 
 export const makeCadastrarUsuario = (): Controller => {
   const repository = new CadastrarUsuariosRepository();
   const service = new CadastrarUsuariosService(repository);
-  return new CadastrarUsuarioController(service);
+  const passwordCrypt = new PasswordCryptMemory();
+  return new CadastrarUsuarioController(service, passwordCrypt);
 }
