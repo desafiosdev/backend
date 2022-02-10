a047f07c5af347500ea949e885b5e3fdde1c9f88
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-02-10 08:48:24 -0300

    feat: create mysql repo for item creation

---
+++ src/repositories/mysql/criar-novo-item-mysql-repository.ts
@@ -0,0 +1,17 @@
+import ICriarNovoItemRepository from '../../contracts/repositories/i-criar-novo-item-repository';
+
+export default class CriarNovoItemMysqlRepository implements ICriarNovoItemRepository {
+  constructor(
+  ) { }
+
+  async execute(usuarioId: string, content: string): Promise<string | Error> {
+    return new Promise((resolve, reject) => {
+      // const itemId = this.connection.driver.escape(this.connection.driver.queryBuilder.connection.createQueryBuilder().insert().into('item').values({
+      //   usuarioId,
+      //   content,
+      // }).getSql());
+      // resolve(itemId);
+      resolve('any_id');
+    });
+  }
+}
