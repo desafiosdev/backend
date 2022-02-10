30e2aaba17a4f3d3c9d961c48c830b9178922257
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-02-10 13:39:56 -0300

    refactor: config url

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import express from 'express';
-import * as path from 'path';
 import { Router } from 'express'
 import addRoutes from './routes';
 
 const app = express();
-const port = process.env.PORT || 3000;
 let router = Router();
 router = addRoutes(router);
+
+const url = process.env.URL || 'http://localhost';
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(router);
 
 app.listen(port, () => {
-  console.log(`App listening on: http://localhost:${port}`)
+  console.log(`App listening on: ${url}:${port}`)
 });
