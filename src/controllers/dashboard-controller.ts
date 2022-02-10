824be6579cf71bd23dd4ba738db4a4ca8563e381
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-02-10 09:04:06 -0300

    feat: add service to dashboard controller

--- src/controllers/dashboard-controller.ts
+++ src/controllers/dashboard-controller.ts
@@ -7,6 +7,9 @@ export default class DashboardController {
   ) { }
 
   async handle(request, response) {
+    localStorage.setItem('message', null);
+    localStorage.setItem('items', JSON.stringify([]));
+
     const userId = localStorage.getItem('userId');
 
     if (!userId) {
@@ -16,14 +19,18 @@ export default class DashboardController {
 
     try {
       const user: { id, name, avatar } = await this.service.getUserById(userId);
-
       if (!localStorage.getItem('name')) localStorage.setItem('name', user.name);
       if (!localStorage.getItem('avatar')) localStorage.setItem('avatar', user.avatar);
+
+      const items = await this.service.getUserItems(userId);
+      localStorage.setItem('items', JSON.stringify(items));
 
       response.sendFile(path.join(__dirname + '/views/dashboard.html'));
 
     } catch (error) {
-      response.sendFile(path.join(__dirname + `/views/dashboard.html?message=${error.message}`));
+      localStorage.setItem('message', error.message);
+
+      response.sendFile(path.join(__dirname + `/views/dashboard.html`));
     }
   }
 
